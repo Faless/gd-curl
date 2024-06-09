d473966fe469cbbdda23ed3be4438aa26bf081cc
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2024-06-09 09:50:27 +0200

    Initial build.

---
+++ src/register_types.cpp
@@ -0,0 +1,67 @@
+/**************************************************************************/
+/*  register_types.cpp                                                    */
+/**************************************************************************/
+/*                         This file is part of:                          */
+/*                             GODOT ENGINE                               */
+/*                        https://godotengine.org                         */
+/**************************************************************************/
+/* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
+/* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
+/*                                                                        */
+/* Permission is hereby granted, free of charge, to any person obtaining  */
+/* a copy of this software and associated documentation files (the        */
+/* "Software"), to deal in the Software without restriction, including    */
+/* without limitation the rights to use, copy, modify, merge, publish,    */
+/* distribute, sublicense, and/or sell copies of the Software, and to     */
+/* permit persons to whom the Software is furnished to do so, subject to  */
+/* the following conditions:                                              */
+/*                                                                        */
+/* The above copyright notice and this permission notice shall be         */
+/* included in all copies or substantial portions of the Software.        */
+/*                                                                        */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
+/**************************************************************************/
+
+#include <gdextension_interface.h>
+
+#include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/core/defs.hpp>
+#include <godot_cpp/godot.hpp>
+
+#ifdef _WIN32
+// See upstream godot-cpp GH-771.
+#undef GDN_EXPORT
+#define GDN_EXPORT __declspec(dllexport)
+#endif
+
+using namespace godot;
+
+void register_webrtc_extension_types(ModuleInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+		return;
+	}
+}
+
+void unregister_webrtc_extension_types(ModuleInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+		return;
+	}
+}
+
+extern "C" {
+GDExtensionBool GDE_EXPORT webrtc_extension_init(const GDExtensionInterfaceGetProcAddress p_interface, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
+	GDExtensionBinding::InitObject init_obj(p_interface, p_library, r_initialization);
+
+	init_obj.register_initializer(register_webrtc_extension_types);
+	init_obj.register_terminator(unregister_webrtc_extension_types);
+	init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
+
+	return init_obj.init();
+}
+}
---
+++ tools/curl.py
@@ -0,0 +1,64 @@
+import os
+
+
+def build_library(env, ssl):
+    curl_config = {
+        "CMAKE_BUILD_TYPE": "RelWithDebInfo" if env["debug_symbols"] else "Release",
+        "BUILD_CURL_EXE": 0,
+        "BUILD_SHARED_LIBS": 0,
+        "BUILD_STATIC_LIBS": 1,
+        "CURL_DISABLE_INSTALL": 1,
+        "CURL_LTO": 1 if env.get("use_lto", False) else 0,
+        "HTTP_ONLY": 1,
+        "BUILD_LIBCURL_DOCS": 0,
+        "BUILD_MISC_DOCS": 0,
+        "ENABLE_CURL_MANUAL": 0,
+        "CURL_DISABLE_OPENSSL_AUTO_LOAD_CONFIG": 0,  # Should this be 1? Nah, probably use mbedTLS in the long run!
+        "CURL_DISABLE_TESTS": 1,
+        "ENABLE_UNIX_SOCKETS": 0,
+        "CURL_CA_BUNDLE": "none",
+        "CURL_CA_PATH": "none",
+        "CURL_USE_LIBSSH2": 0,  # Why does curl even...
+        "CURL_USE_LIBPSL": 0,  # Thanks IANA.
+        "USE_LIBIDN2": 0,  # GPL
+        "CMAKE_POSITION_INDEPENDENT_CODE": 1,
+        "CMAKE_DISABLE_FIND_PACKAGE_ZLIB": 1,
+        "OPENSSL_USE_STATIC_LIBS": 1,
+        "OPENSSL_INCLUDE_DIR": env["SSL_INCLUDE"],
+        "OPENSSL_SSL_LIBRARY": env["SSL_LIBRARY"],
+        "OPENSSL_CRYPTO_LIBRARY": env["SSL_CRYPTO_LIBRARY"],
+        "OPENSSL_ROOT_DIR": env["SSL_INSTALL"],
+    }
+    is_msvc = env.get("is_msvc", False)
+    lib_ext = ".lib" if is_msvc else ".a"
+    lib_prefix = "" if is_msvc else "lib"
+    curl_libs = [
+        "lib/{}curl{}".format(lib_prefix, lib_ext),
+    ]
+    # Build cURL
+    curl = env.CMakeBuild(
+        "#bin/thirdparty/curl/",
+        "#thirdparty/curl",
+        cmake_options=curl_config,
+        cmake_outputs=curl_libs,
+        cmake_targets=["libcurl_static"],
+        dependencies=ssl,
+    )
+
+    # Configure env.
+    if env["platform"] == "windows":
+        env.PrependUnique(LIBS=["ws2_32", "bcrypt"])
+    if env["platform"] == "linux":
+        env.PrependUnique(LIBS=["pthread"])
+    env.Prepend(LIBS=list(filter(lambda f: str(f).endswith(lib_ext), curl)))
+    env.Append(CPPPATH=["#thirdparty/curl/include"])
+
+    return curl
+
+
+def exists(env):
+    return "CMake" in env
+
+
+def generate(env):
+    env.AddMethod(build_library, "BuildCURL")
