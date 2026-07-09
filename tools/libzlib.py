d683330693cf13da1c5c1d174c05ebfd8a6f1454
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2026-07-09 18:21:25 +0200

    Update dependencies and build tools

--- tools/libzlib.py
+++ tools/libzlib.py
@@ -1,9 +1,9 @@
 def build_library(env):
-    zlib_bin = env.Dir("#bin/thirdparty/zlib/{}/{}/install".format(env["platform"], env["arch"]))
-    is_msvc = env.get("is_msvc", False)
+    zlib_bin = env.Dir("bin/thirdparty/zlib/{}/{}/install".format(env["platform"], env["arch"]))
     zlib_config = {
         "CMAKE_BUILD_TYPE": "RelWithDebInfo" if env["debug_symbols"] else "Release",
-        "ZLIB_BUILD_EXAMPLES": 0,
+        "ZLIB_BUILD_TESTING": 0,
+        "ZLIB_BUILD_SHARED": 0,
         "CMAKE_INSTALL_PREFIX": env.Dir(zlib_bin).abspath,
         "CMAKE_INSTALL_LIBDIR": "lib",
     }
@@ -12,9 +12,9 @@ def build_library(env):
         # This is needed on some arch when building with the godot buildroot toolchain
         zlib_config["CMAKE_C_FLAGS"] = " -fPIC"
 
-    lib_name = "zlibstatic" if env["platform"] == "windows" else "z"
-    lib_ext = ".lib" if is_msvc else ".a"
-    lib_prefix = "" if is_msvc else "lib"
+    lib_name = "zs" if env["platform"] == "windows" else "z"
+    lib_ext = ".lib" if env.msvc else ".a"
+    lib_prefix = "" if env.msvc else "lib"
     zlib_libs = [
         "/install/lib/{}{}{}".format(lib_prefix, lib_name, lib_ext),
     ]
@@ -26,14 +26,15 @@ def build_library(env):
 
     # Build libdatachannel
     zlib = env.CMakeBuild(
-        env.Dir("#bin/thirdparty/zlib/"),
-        env.Dir("#thirdparty/zlib"),
+        env.Dir("bin/thirdparty/zlib/"),
+        env.Dir("thirdparty/zlib"),
         cmake_options=zlib_config,
         cmake_outputs=zlib_libs + zlib_includes,
+        cmake_targets=[],
         install=True,
     )
     env.Prepend(LIBS=list(filter(lambda f: str(f).endswith(lib_ext), zlib)))
-    env.Append(CPPPATH=[env.Dir("#thirdparty/zlib/include")])
+    env.Append(CPPPATH=[env.Dir("thirdparty/zlib/include")])
 
     return zlib
 
@@ -43,11 +44,10 @@ def exists(env):
 
 
 def generate(env):
-    zlib_install_dir = "#bin/thirdparty/zlib/{}/{}/install".format(env["platform"], env["arch"])
-    is_msvc = env.get("is_msvc", False)
+    zlib_install_dir = "bin/thirdparty/zlib/{}/{}/install".format(env["platform"], env["arch"])
     lib_name = "zlibstatic" if env["platform"] == "windows" else "z"
-    lib_prefix = "" if is_msvc else "lib"
-    lib_ext = ".lib" if is_msvc else ".a"
+    lib_prefix = "" if env.msvc else "lib"
+    lib_ext = ".lib" if env.msvc else ".a"
     zlib = env.File(zlib_install_dir + "/lib/{}{}{}".format(lib_prefix, lib_name, lib_ext))
     includes = env.Dir(zlib_install_dir + "/include")
     env.AddMethod(build_library, "BuildZlib")
--- tools/mbedtls.py
+++ tools/mbedtls.py
@@ -1,11 +1,10 @@
 def build_library(env):
-    mbedtls_bin = env.Dir("#bin/thirdparty/mbedtls/{}/{}/install".format(env["platform"], env["arch"]))
-    is_msvc = env.get("is_msvc", False)
+    mbedtls_bin = env.Dir("bin/thirdparty/mbedtls/{}/{}/install".format(env["platform"], env["arch"]))
     c_flags = "-DMBEDTLS_SSL_DTLS_SRTP"
     if env["platform"] == "linux":
         # This is needed on some arch when building with the godot buildroot toolchain
         c_flags += " -fPIC"
-    elif env["platform"] == "windows" and not is_msvc:
+    elif env["platform"] == "windows" and not env.msvc:
         c_flags += " -D__USE_MINGW_ANSI_STDIO=0"  # See https://github.com/Mbed-TLS/mbedtls/issues/10161
 
     mbedtls_config = {
@@ -17,8 +16,8 @@ def build_library(env):
         "MBEDTLS_FATAL_WARNINGS": 0,
         "CMAKE_INSTALL_LIBDIR": "lib",
     }
-    lib_ext = ".lib" if is_msvc else ".a"
-    lib_prefix = "" if is_msvc else "lib"
+    lib_ext = ".lib" if env.msvc else ".a"
+    lib_prefix = "" if env.msvc else "lib"
     mbedtls_libs = [
         "/install/lib/{}mbedtls{}".format(lib_prefix, lib_ext),
         "/install/lib/{}mbedx509{}".format(lib_prefix, lib_ext),
@@ -33,8 +32,8 @@ def build_library(env):
 
     # Build libdatachannel
     mbedtls = env.CMakeBuild(
-        env.Dir("#bin/thirdparty/mbedtls/"),
-        env.Dir("#thirdparty/mbedtls"),
+        env.Dir("bin/thirdparty/mbedtls/"),
+        env.Dir("thirdparty/mbedtls"),
         cmake_options=mbedtls_config,
         cmake_outputs=mbedtls_libs + mbedtls_cmake_config,
         install=True,
@@ -46,7 +45,7 @@ def build_library(env):
     if env["platform"] == "linux":
         env.PrependUnique(LIBS=["pthread"])
     env.Prepend(LIBS=list(filter(lambda f: str(f).endswith(lib_ext), mbedtls)))
-    env.Append(CPPPATH=[env.Dir("#thirdparty/mbedtls/include")])
+    env.Append(CPPPATH=[env.Dir("thirdparty/mbedtls/include")])
 
     return mbedtls
 
@@ -56,12 +55,12 @@ def exists(env):
 
 
 def generate(env):
-    mbedtls_install_dir = "#bin/thirdparty/mbedtls/{}/{}/install".format(env["platform"], env["arch"])
-    lib_ext = ".lib" if env.get("is_msvc", False) else ".a"
+    mbedtls_install_dir = "bin/thirdparty/mbedtls/{}/{}/install".format(env["platform"], env["arch"])
+    lib_ext = ".lib" if env.msvc else ".a"
     mbedtls = env.File(mbedtls_install_dir + "/lib/libmbedtls" + lib_ext)
     crypto = env.File(mbedtls_install_dir + "/lib/libmbedcrypto" + lib_ext)
     x509 = env.File(mbedtls_install_dir + "/lib/libmbedx509" + lib_ext)
-    includes = env.Dir("#thirdparty/mbedtls/include")
+    includes = env.Dir("thirdparty/mbedtls/include")
     env.AddMethod(build_library, "BuildMbedTLS")
     env["MBEDTLS_LIBRARY"] = mbedtls.abspath
     env["MBEDTLS_CRYPTO_LIBRARY"] = crypto.abspath
